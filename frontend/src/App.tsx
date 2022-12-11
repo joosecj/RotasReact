828b6c77d15b639af7e3aeb4b3f34dd49c042b67
Jose Carlos <josecarloscjj@gmail.com>
2022-12-11 17:30:23 -0300

    fix: route home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './routes/Home'
+import HomeBody from './routes/Home/HomeBody'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} >
         <Route index element={<Navigate to="/home" />} />
+        <Route path='home' element={<HomeBody/>} />
       </Route>
     </Routes>
   </BrowserRouter>
---
+++ frontend/src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,11 @@
+import HomeCard from "../../../components/Header/HomeCard";
+
+export default function HomeBody() {
+  return (
+    <main>
+      <section className="container">
+        <HomeCard title="Bem Vindo!" />
+      </section>
+    </main>
+  );
+}
